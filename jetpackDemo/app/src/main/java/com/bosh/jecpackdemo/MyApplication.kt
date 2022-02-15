cd8df9df53dac660022484ed161305a495c06a6d
bosh <810916259@qq.com>
2022-02-15 14:58:08 +0800

    引入hilt、lifecycle、startup、multiDex依赖 使用java11 使用compileSdk 31

---
+++ jetpackDemo/app/src/main/java/com/bosh/jecpackdemo/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.bosh.jecpackdemo
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author lzq
+ * @date  2022/2/15
+ */
+@HiltAndroidApp
+class MyApplication : MultiDexApplication() {
+}
